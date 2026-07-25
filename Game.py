5b7f47f29ec8101855e1364cf80b86cd4d640a53
JakeRazzembly The Coder <erinrazzembly@gmail.com>
2026-07-25 11:39:56 +1000

    Fixed the _input_get() Noticed i built it for a text not an entry

--- Game.py
+++ Game.py
@@ -3,17 +3,17 @@ import tkinter as tk
 Saved_Name = ""
 
 def _input_get(input, output):
-    input = tk.Text.get(output)
+    output = input.get()
     return(output)
-    print(Saved_Name)
 
 root = tk.Tk()
 
 root.title("Ocean Quiz")
 
-Name_Input = tk.Text()
+Name_Input = tk.Entry()
 Name_Input.grid(column=0, row=0, padx=20, pady=30,)
 
-Save_Name = tk.Button(text="Enter Name", function=lambda: _input_get(Name_Input, Saved_Name))
+Save_Name = tk.Button(text="Enter Name", command=lambda: _input_get(Name_Input, Saved_Name))
+Save_Name.grid(column=1, row=0, padx=20, pady=30,)
 
 root.mainloop()
