11740853d9193dd019b3421616615de32c18c25d
JakeRazzembly The Coder <erinrazzembly@gmail.com>
2026-07-25 11:29:44 +1000

    Added System For Saving Text Outputs

--- Game.py
+++ Game.py
@@ -1,7 +1,19 @@
 import tkinter as tk
 
+Saved_Name = ""
+
+def _input_get(input, output):
+    input = tk.Text.get(output)
+    return(output)
+    print(Saved_Name)
+
 root = tk.Tk()
 
-root.title("OceanQuiz")
+root.title("Ocean Quiz")
+
+Name_Input = tk.Text()
+Name_Input.grid(column=0, row=0, padx=20, pady=30,)
+
+Save_Name = tk.Button(text="Enter Name", function=lambda: _input_get(Name_Input, Saved_Name))
 
 root.mainloop()
