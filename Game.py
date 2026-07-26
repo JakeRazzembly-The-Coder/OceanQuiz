15f942c039621fab0478392935f8da5201bcccf2
JakeRazzembly The Coder <erinrazzembly@gmail.com>
2026-07-27 09:49:39 +1000

    Added a error for empty text input

--- Game.py
+++ Game.py
@@ -2,18 +2,27 @@ import tkinter as tk
 
 Saved_Name = ""
 
-def _input_get(input, output):
-    output = input.get()
-    return(output)
+def _input_get(input, output, error):
+    output = input.get().strip()
+    if output == "":
+        error.set("Enter a Text")
+    else:
+        return(output)
 
 root = tk.Tk()
 
+Name_Error_TextVar = tk.StringVar(root, value="")
+
 root.title("Ocean Quiz")
 
-Name_Input = tk.Entry()
-Name_Input.grid(column=0, row=0, padx=20, pady=30,)
+Name_Input = tk.Entry(root,)
 
-Save_Name = tk.Button(text="Enter Name", command=lambda: _input_get(Name_Input, Saved_Name))
-Save_Name.grid(column=1, row=0, padx=20, pady=30,)
+Save_Name = tk.Button(text="Enter Name", command=lambda: _input_get(Name_Input, Saved_Name, Name_Error_TextVar,))
+
+Name_Error = tk.Label(root, textvariable=Name_Error_TextVar,)
+
+Name_Input.grid(column=0, row=0, padx=20, pady=(30, 5))
+Save_Name.grid(column=1, row=0, padx=20, pady=(30, 5))
+Name_Error.grid(column=0, row=1, padx=20, pady=(0, 30))
 
 root.mainloop()
